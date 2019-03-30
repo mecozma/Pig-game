eafd2ca506f90f999835e40be69a25eaa264459c
mecozma <mecozma@gmail.com>
2019-03-30 15:33:15 +0000

    Added functionality yo update de score and to toggle the active player

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ activePlayer = 0;
 
 // Reset the game
 document.querySelector(".dice").style.display = "none";
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 // Event listeners
 document.querySelector(".btn-roll").addEventListener("click", () => {
@@ -31,7 +31,28 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
   diceDOM.style.display = "block";
   diceDOM.src = `dice-${dice}.png`;
   // 3. Update the round score IF the rolled number was not a 1.
-});
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //Next player turn
+    // if (dice === 1) {
+    //   dice = 0;
+    // } else {
+    //   dice = 1;
+    // }
+    (activePlayer == 0) ? activePlayer = 1 : activePlayer = 0;
 
+    roundScore = 0;
+
+    document.getElementById("current-" + activePlayer).textContent = "0";
+
+    document.querySelector(`.player-0-panel`).classList.toggle('active');
+    document.querySelector(`.player-1-panel`).classList.toggle('active');
+
+    document.querySelector(".dice").style.display = "none";
+  }
+});
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
