d1d70c161439f7a1d30c5e5555bece6d29d44fea
mecozma <mecozma@gmail.com>
2019-03-30 14:44:25 +0000

    Added event listeners and functionality to update the dice

--- app.js
+++ app.js
@@ -9,13 +9,29 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+// Reset the game
+document.querySelector(".dice").style.display = "none";
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// Event listeners
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  // 1. Random number;
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display the result;
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = `dice-${dice}.png`;
+  // 3. Update the round score IF the rolled number was not a 1.
+});
+
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
